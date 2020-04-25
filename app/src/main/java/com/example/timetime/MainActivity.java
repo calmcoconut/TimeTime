936356bfcd586e720637e171ee15b8e3bd9efcef
Alejandro <diaz.alejandro772@gmail.com>
2020-04-25 11:31:29 +0900

    Added icons in raw

--- app/src/main/java/com/example/timetime/MainActivity.java
+++ app/src/main/java/com/example/timetime/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.timetime;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.timetime.database.database.FirstTimeDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstTimeDatabase test = new FirstTimeDatabase();
     }
 }
