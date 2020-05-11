0f033a0c152e3260185d931fa09ade01021522ec
Alejandro <diaz.alejandro772@gmail.com>
2020-05-11 20:27:06 +0900

    Implementing UIs for categories and Activities

--- app/src/main/java/com/example/timetime/ui/categorysummary/CategoryFragment.java
+++ app/src/main/java/com/example/timetime/ui/categorysummary/CategoryFragment.java
@@ -4,26 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.timetime.R;
+import com.example.timetime.database.entity.Category;
+import com.example.timetime.viewmodels.CategoryViewModel;
+
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
+    private CategoryViewModel mCategoryViewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        final View rootView = inflater.inflate(R.layout.recyclerview_item, container, false);
-        TextView textView = rootView.findViewById(R.id.time_card_recycler_title);
-        textView.setText("THIS IS CATEGORY FRAGMENT");
+        final View rootView = inflater.inflate(R.layout.recycler_view, container, false);
+        startRecyclerForCategory(rootView);
         return rootView;
     }
 
+    private void startRecyclerForCategory(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_root);
 
+        final CategoryListAdapter adapter = new CategoryListAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        mCategoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        mCategoryViewModel.getAllCategories().observe(getViewLifecycleOwner(),
+                new Observer<List<Category>>() {
+                    @Override
+                    public void onChanged(@Nullable final List<Category> categories) {
+                        adapter.setCategories(categories);
+                    }
+                });
+    }
 
     // factory method for returning an instance of the class
     public static CategoryFragment newInstance() {
