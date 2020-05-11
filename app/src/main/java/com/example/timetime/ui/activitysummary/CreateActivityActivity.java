3bd9e8e159e5a78cf4e8c3637a14db116a5bfcbb
Alejandro <diaz.alejandro772@gmail.com>
2020-05-11 19:16:29 +0900

    Found solution to different onclick action for the main Fab.  Now implementing on clicks.

---
+++ app/src/main/java/com/example/timetime/ui/activitysummary/CreateActivityActivity.java
@@ -0,0 +1,14 @@
+package com.example.timetime.ui.activitysummary;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.timetime.R;
+
+public class CreateActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_activity);
+    }
+}
