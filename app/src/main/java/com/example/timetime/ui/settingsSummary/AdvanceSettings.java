5772bc46a8cc2ebdec3f234f9a39c177245e3d33
Alejandro <diaz.alejandro772@gmail.com>
2020-08-13 14:04:26 +0900

    finished setting up settings page

--- app/src/main/java/com/example/timetime/ui/settingsSummary/AdvanceSettings.java
+++ app/src/main/java/com/example/timetime/ui/settingsSummary/AdvanceSettings.java
@@ -7,6 +7,14 @@ import androidx.preference.*;
 
 public class AdvanceSettings extends PreferenceFragmentCompat {
     Context context;
+    SwitchPreferenceCompat pushNotification;
+    EditTextPreference pushNotificationInterval;
+    SwitchPreferenceCompat lockScreenNotification;
+    EditTextPreference lockScreenNotificationInterval;
+    ListPreference timeFormat;
+    Preference exportDatabase;
+    EditTextPreference deleteDatabase;
+
     PreferenceScreen screen;
 
     @Override
@@ -14,62 +22,82 @@ public class AdvanceSettings extends PreferenceFragmentCompat {
         context = getPreferenceManager().getContext();
         screen = getPreferenceManager().createPreferenceScreen(context);
 
-        setNotificationSettings();
-        setOtherSettings();
+        setNotificationCategory();
+        setOtherCategory();
 
         setPreferenceScreen(screen);
+        setDependencies();
     }
 
-    private void setNotificationSettings() {
+    private void setDependencies() {
+        pushNotificationInterval.setDependency(pushNotification.getKey());
+        lockScreenNotificationInterval.setDependency(lockScreenNotification.getKey());
+    }
+
+    private void setNotificationCategory() {
         PreferenceCategory notificationCategory = new PreferenceCategory(context);
         notificationCategory.setKey("notification_category");
         notificationCategory.setTitle("Notifications");
         screen.addPreference(notificationCategory);
 
-        SwitchPreferenceCompat pushNotification = new SwitchPreferenceCompat(context);
-        pushNotification.setKey("push_notifications");
+        pushNotification = new SwitchPreferenceCompat(context);
+        pushNotification.setKey("push_notification");
         pushNotification.setTitle("Enable push notifications");
         pushNotification.setChecked(true);
         notificationCategory.addPreference(pushNotification);
 
-        EditTextPreference pushNotificationInterval = new EditTextPreference(context);
+        pushNotificationInterval = new EditTextPreference(context);
         pushNotificationInterval.setKey("push_notifications_interval");
         pushNotificationInterval.setTitle("Push notification frequency");
         pushNotificationInterval.setSummaryProvider(provider -> pushNotificationInterval.getText() + " minutes");
         pushNotificationInterval.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_NUMBER));
+        pushNotificationInterval.setDialogTitle("Choose how often to be notified");
         pushNotificationInterval.setDefaultValue("30");
         notificationCategory.addPreference(pushNotificationInterval);
 
-        SwitchPreferenceCompat lockScreenNotification = new SwitchPreferenceCompat(context);
+        lockScreenNotification = new SwitchPreferenceCompat(context);
         lockScreenNotification.setKey("lock_screen_notifications");
         lockScreenNotification.setTitle("Enable Lock Screen notifications");
         notificationCategory.addPreference(lockScreenNotification);
 
-        EditTextPreference lockScreenNotificationInterval = new EditTextPreference(context);
+        lockScreenNotificationInterval = new EditTextPreference(context);
         lockScreenNotificationInterval.setKey("lock_screen_notifications_interval");
         lockScreenNotificationInterval.setTitle("Lock screen notification frequency");
         lockScreenNotificationInterval.setSummaryProvider(provider -> lockScreenNotificationInterval.getText() + " minutes");
         lockScreenNotificationInterval.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_NUMBER));
+        lockScreenNotificationInterval.setDialogTitle("Choose how often to be notified");
         lockScreenNotificationInterval.setDefaultValue("30");
         notificationCategory.addPreference(lockScreenNotificationInterval);
     }
 
-    private void setOtherSettings() {
+    private void setOtherCategory() {
         PreferenceCategory otherCategory = new PreferenceCategory(context);
         otherCategory.setKey("other_category");
         otherCategory.setTitle("Other");
         screen.addPreference(otherCategory);
 
-        Preference timeFormat = new Preference(context);
+        timeFormat = new ListPreference(context);
+        timeFormat.setKey("time_format");
+        timeFormat.setTitle("Time format");
+        timeFormat.setEntries(new CharSequence[]{"12 HOUR", "24 HOUR"});
+        timeFormat.setEntryValues(new CharSequence[]{"12", "24"});
+        timeFormat.setSummary("Configure to use a 12 hour format (1:00 PM) or a 24 hour format (13:00)");
+        timeFormat.setDialogTitle("Choose a time format");
+        timeFormat.setDefaultValue("24");
+        otherCategory.addPreference(timeFormat);
 
-        Preference exportDatabase = new Preference(context);
+        exportDatabase = new Preference(context);
         exportDatabase.setKey("export_data");
-        exportDatabase.setTitle("Export Database");
-        exportDatabase.setSummary("export database to a local file for use externally (excel, backup, etc.)");
+        exportDatabase.setTitle("Export database");
+        exportDatabase.setSummary("export database to a local file for external use");
+        otherCategory.addPreference(exportDatabase);
 
-        Preference deleteDatabase = new Preference(context);
+        deleteDatabase = new EditTextPreference(context);
         deleteDatabase.setKey("delete_data");
-        deleteDatabase.setTitle("Delete Database");
+        deleteDatabase.setTitle("Delete database");
+        deleteDatabase.setDialogTitle("TYPE DELETE TO CONFIRM");
+
+        otherCategory.addPreference(deleteDatabase);
     }
 
     public AdvanceSettings() {
